7622e0006c4a5ccf209054fdb2f765d752ad9f92
ngohungphuc <ngohungphuc95@gmail.com>
2017-10-17 16:23:46 +0700

    add hash helper

---
+++ OnlineTraining/OnlineTraining.Helper/Hash/HashHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OnlineTraining.Helper.Hash
+{
+    public static class HashHelper
+    {
+        public static string Sha512(string input)
+        {
+            using (var sha = SHA512.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(input);
+                var hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
