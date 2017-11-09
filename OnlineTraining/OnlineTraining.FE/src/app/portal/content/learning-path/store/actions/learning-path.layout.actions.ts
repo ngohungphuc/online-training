d09a226fae8394f585cccebbbea268aab6e68ef8
ngohungphuc <ngohungphuc95@gmail.com>
2017-11-09 14:23:16 +0700

    add mising payload for layout

--- OnlineTraining/OnlineTraining.FE/src/app/portal/content/learning-path/store/actions/learning-path.layout.actions.ts
+++ OnlineTraining/OnlineTraining.FE/src/app/portal/content/learning-path/store/actions/learning-path.layout.actions.ts
@@ -4,6 +4,8 @@ export const DETAIL_PAGE = 'DETAIL_PAGE';
 
 export class DetailPage implements Action {
     readonly type = DETAIL_PAGE;
+    constructor(public payload: boolean) {
+    }
 }
 
 export type Actions = DetailPage;
